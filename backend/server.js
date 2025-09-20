685a4cf99ca841727434995341acf2f4e8b6026d
Chetana <nachi0449@gmail.com>
2025-09-20 10:59:26 +0530

    updated the .env

--- backend/server.js
+++ backend/server.js
@@ -7,18 +7,17 @@ import productRoutes from "./routes/productRoutes.js";
 import cartRoutes from "./routes/cartRoutes.js";
 
 const app = express();
-app.use(cors({origin:"*"}));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB connected"))
-.catch((err) => console.log("MongoDB error:", err));
-console.log("MONGO_URI:", process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.error("❌ MongoDB error:", err));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -26,4 +25,3 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
-
